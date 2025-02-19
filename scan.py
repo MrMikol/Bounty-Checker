ea188fa046298c22f1a4c1ebd6523b6642221606
Michael Tan <michaelneiltan04@gmail.com>
2025-02-19 15:05:24 -0400

    Added loops to a url txt

--- scan.py
+++ scan.py
@@ -2,25 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 
 def check(url: str, word: str, page) -> int:
-    full_url = url + f"&page={page}"
-    print(full_url)
-    try:
-        response = requests.get(full_url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching the pageL {e}")
-        return 0
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
-    count = text.lower().count(word.lower())
-
+    while page < 4:
+        full_url = url + f"&page={page}"
+        print(full_url)
+        try:
+            response = requests.get(full_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching the pageL {e}")
+            return 0
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        count = text.lower().count(word.lower())
+        page+=1
+        print(f"{word} appears {count} times.")
+    print()
     return count
-page = [1, 2, 3]
-url = "https://polkadot.subsquare.io/treasury/child-bounties?parentBountyId=50"
+page = 1
+url = "urls.txt"
 word = "lucky"
 
-for page in page:
+#for page in page:
 
-    count = check(url, word, page)
-    print(f"{word} appears {count} times.")
+    #count = check(url, word, page)
+    #print(f"{word} appears {count} times.")
+
+try:
+    with open(url, "r") as file:
+        site_urls = [line.strip() for line in file if line.strip()]
+except FileNotFoundError:
+    print(f"Error: {url} not found!")
+    exit()
+print("File Opened")
+
+for site_url in site_urls:
+    count = check(site_url, word, page)
